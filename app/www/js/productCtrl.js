9b08560d76854612c0682a3497d0ba52a279541f
Siggysic <siggysic@gmail.com>
2016-03-04 21:39:52 +0700

    Add product and menu view

--- app/www/js/productCtrl.js
+++ app/www/js/productCtrl.js
@@ -1,11 +1,30 @@
 angular.module('starter.productCtrl', [])
 
-	.controller('PlaylistsCtrl', function($scope, $http) {
-	  $http.get('/products')
-	    .success(function(response) {
-	      console.log(response);
-	      $scope.playlists = response;
-	    });
+	.controller('ProductCtrl', function($scope, $http) {
+		$scope.dataTest = [{
+					"name" : "กระเป็า",
+					"detail" : "ก็ลองดูจ่าาาาาาาาาาาาา"
+				},
+				{
+					"name" : "5555",
+					"detail" : "5555555555555555"
+				},
+				{
+					"name" : "5555",
+					"detail" : "5555555555555555"
+				},
+				{
+					"name" : "5555",
+					"detail" : "5555555555555555"
+				}]
+				$scope.slideChanged = function(index) {
+    $scope.slideIndex = index;
+  };
+	  // $http.get('/products')
+	  //   .success(function(response) {
+	  //     console.log(response);
+	  //     $scope.playlists = response;
+	  //   });
 	})
 
 	.controller('PlaylistCtrl', function($scope, $stateParams) {
